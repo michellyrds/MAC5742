70c68187eac374f2bb8667ae278b32e8000ba3d2
Michelly <michelly.silva753@gmail.com>
2024-11-18 20:35:22 -0300

    add ep2/run_measurements.sh

---
+++ ep2/run_measurements.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+set -o xtrace
+
+MEASUREMENTS=20
+ITERATIONS=8
+
+INITIAL_SIZE=16
+N_POINTS=100
+ITER_LIMIT=1024
+
+THREADS_PER_BLOCK=$INITIAL_SIZE
+
+NAMES=('heat' 'cuda_heat')
+
+make all
+mkdir -p results
+
+for NAME in ${NAMES[@]}; do
+    mkdir -p results/$NAME
+
+    for ((i=1; i<=$ITERATIONS; i++)); do
+            BLOCK_SIZE=$(echo "sqrt($THREADS_PER_BLOCK)" | bc)
+            GRID_DIM=$((($N_POINTS + $BLOCK_SIZE - 1) / $BLOCK_SIZE ))
+            BLOCKS_PER_GRID=$(( GRID_DIM * GRID_DIM ))
+            perf stat -r $MEASUREMENTS ./$NAME.o $N_POINTS $ITER_LIMIT $THREADS_PER_BLOCK $BLOCKS_PER_GRID >> $NAME.log 2>&1
+            THREADS_PER_BLOCK=$(($THREADS_PER_BLOCK * 2))
+    done
+
+    THREADS_PER_BLOCK=$INITIAL_SIZE
+
+    mv *.log results/$NAME
+done
+
+rm -f room.txt cuda_room.txt

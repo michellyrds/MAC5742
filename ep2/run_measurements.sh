1ff871883edca77164694968d9d5ea4ba50e3143
Michelly <michelly.silva753@gmail.com>
2024-11-19 01:00:37 -0300

    update run_measurements.sh

--- ep2/run_measurements.sh
+++ ep2/run_measurements.sh
@@ -3,9 +3,9 @@
 set -o xtrace
 
 MEASUREMENTS=20
-ITERATIONS=10
+ITERATIONS=8
 
-INITIAL_SIZE=16
+INITIAL_SIZE=2
 N_POINTS=100
 ITER_LIMIT=1024
 
@@ -26,7 +26,7 @@ for NAME in ${NAMES[@]}; do
         for ((j=1; j<=$MEASUREMENTS; j++)); do
             nvprof ./$NAME.o $N_POINTS $ITER_LIMIT $THREADS_PER_BLOCK $BLOCKS_PER_GRID --cpu-profiling >> $NAME.log 2>&1
         done
-        THREADS_PER_BLOCK=$(($THREADS_PER_BLOCK * 2))
+        THREADS_PER_BLOCK=$(($THREADS_PER_BLOCK * $THREADS_PER_BLOCK))
     done
 
     THREADS_PER_BLOCK=$INITIAL_SIZE
